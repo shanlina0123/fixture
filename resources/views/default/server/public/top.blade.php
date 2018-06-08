598b6a2678d1bd782bd40c95895dde829880a119
单莉娜 <1161110682@qq.com>
2018-06-08 17:56:25 +0800

    帮助文档

--- resources/views/default/server/public/top.blade.php
+++ resources/views/default/server/public/top.blade.php
@@ -1,17 +1,20 @@
 <div class="layui-logo"><a href="{{route('index')}}"><img src="{{pix_asset('server/images/topLogo.png')}}"></a></div>
 <ul class="layui-nav layui-layout-right">
-    <li class="layui-nav-item">
-        <a href="{{route('vip-index')}}" title="会员中心"><img src="{{pix_asset('server/images/user.png')}}" width="32"></a>
+    <li class="layui-nav-item" >
+        <a href="http://showdoc.yygsoft.com/web/#/page/1" title="帮助中心" style="padding: 0 15px;"><img src="{{pix_asset('server/images/help.png')}}" width="32"></a>
     </li>
-    <li class="layui-nav-item">
-        <a href="{{route('notice-index')}}" title="通知消息" url="{{route('notice-listen',strtotime(date('Y-m-d H:i:s')))}}" id="notice-listen"><img src="{{pix_asset('server/images/notice.png')}}" width="32"><span class="layui-badge-dot" style="position: relative; top: -3px; left: -3px;display: none;"></span></a>
+    <li class="layui-nav-item" >
+        <a href="{{route('vip-index')}}" title="会员中心" style="padding: 0 15px;"><img src="{{pix_asset('server/images/user.png')}}" width="32"></a>
     </li>
-    <li class="layui-nav-item">
-        <a href="{{route("message-index")}}" title="咨询消息"><img src="{{pix_asset('server/images/message.png')}}" width="32"><span class="layui-badge-dot" style="position: relative; top: -3px; left: -3px;"></span></a>
+    <li class="layui-nav-item" >
+        <a href="{{route('notice-index')}}" title="通知消息" style="padding: 0 15px;" style="padding: 0 15px;"url="{{route('notice-listen',strtotime(date('Y-m-d H:i:s')))}}" id="notice-listen"><img src="{{pix_asset('server/images/notice.png')}}" width="32"><span class="layui-badge-dot" style=" top: 27px; left: 35px;"></span></a>
     </li>
-    <li class="layui-nav-item">
-        <a href="javascript:;">
-            <img src="{{pix_asset('server/images/default.png')}}" class="layui-nav-img">{{session("userInfo")->username}}
+    <li class="layui-nav-item" >
+        <a href="{{route("message-index")}}" title="咨询消息" style="padding: 0 15px;"><img src="{{pix_asset('server/images/message.png')}}" width="32"><span class="layui-badge-dot" style="top: 27px; left: 40px;"></span></a>
+    </li>
+    <li class="layui-nav-item" >
+        <a href="javascript:;" style="padding: 0 15px;">
+            <img src="{{pix_asset('server/images/default.png')}}" class="layui-nav-img" >{{session("userInfo")->username}}
         </a>
         <dl class="layui-nav-child">
             <dd><a href="{{route('user-info')}}">个人资料</a></dd>
