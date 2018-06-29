2ec860a0cfd6fa896dfee38f2712eea41e24b5fb
单莉娜 <1161110682@qq.com>
2018-06-29 17:38:23 +0800

    vip权限控制。接口端的代码已填写，展示注视着，使用时候再打开。

--- app/Http/Middleware/AuthCheck.php
+++ app/Http/Middleware/AuthCheck.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 use App\Http\Model\Filter\FilterFunction;
 use App\Http\Model\Filter\FilterRoleFunction;
+use App\Http\Model\Vip\ConfVipfunctionpoint;
 use App\Model\Data\Functions;
 use App\Model\Roles\RoleFunction;
 use Closure;
@@ -12,6 +13,7 @@ class AuthCheck
 {
     public function handle($request, Closure $next)
     {
+
         //获取当前登录用户信息
         $admin_user= $request->session()->get('userInfo');//对象
 
@@ -24,6 +26,13 @@ class AuthCheck
                 return redirect()->route('error-lock')->with('msg', '无权限');
             }
        }
+        //验证VIP权限
+        $vipflag= $this->authVip($admin_user);
+        if($vipflag==false)
+        {
+            return redirect()->route('vip-index')->with('msg', '无权限,请升级为专业版');
+        }
+
         return $next($request);
     }
 
@@ -58,7 +67,37 @@ class AuthCheck
         if (empty($authControler->toArray())) {
             return  false;
         }
+        return true;
 
+    }
+
+    /***
+     * VIP访问权限验证
+     * @param $admin_user
+     * @return bool
+     */
+    protected function authVip($admin_user)
+    {
+        //当前访问的控制器和方法
+        $current=getCurrentAction();
+        //当前访问控制器
+        $routeController = $current["controller"];
+
+        //vip-标准版权限访问权
+        if($admin_user["vipmechanismid"]==1)
+        {
+            //vip限制的控制器
+            $vipData=ConfVipfunctionpoint::where("status",1)->where("type","has")->where("controller",$routeController)->first();
+            if($vipData)
+            {
+                //vip对限制控制器的访问限制
+                if(!$vipData["value"])
+                {
+                    return  false;
+                }
+            }
+
+        }
         return true;
 
     }
--- upgrade/database/v1.0.0.1.sql
+++ upgrade/database/v1.0.0.1.sql
@@ -1,4 +1,5 @@
-SET FOREIGN_KEY_CHECKS=0;
+#授权
+ALTER TABLE `fixture_small_program` MODIFY COLUMN `sourcecode`  tinyint(1) NULL DEFAULT NULL COMMENT '1审核成功 0代码提交失败 2.审核中  3.审核失败' AFTER `uploadcode`;
 #增加公司封面背景图
 ALTER TABLE `fixture_company` ADD COLUMN `covermap`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '封面图' AFTER `deadline`;
 #会员机制
@@ -6,40 +7,34 @@ ALTER TABLE `fixture_conf_vipfunctionpoint` ADD COLUMN `text`  varchar(255) CHAR
 ALTER TABLE `fixture_conf_vipfunctionpoint` ADD COLUMN `value`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标准版value' AFTER `text`;
 ALTER TABLE `fixture_conf_vipfunctionpoint` ADD COLUMN `vipvalue`  varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'vip版value' AFTER `value`;
 ALTER TABLE `fixture_conf_vipfunctionpoint` ADD COLUMN `viptext`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL AFTER `vipvalue`;
-ALTER TABLE `fixture_conf_vipfunctionpoint` MODIFY COLUMN `content`  longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '说明' AFTER `viptext`;
+ALTER TABLE `fixture_conf_vipfunctionpoint` MODIFY COLUMN `content`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '说明' AFTER `viptext`;
 ALTER TABLE `fixture_conf_vipfunctionpoint` DROP COLUMN `vipmechanismid`;
-#授权
-ALTER TABLE `fixture_small_program` MODIFY COLUMN `sourcecode`  tinyint(1) NULL DEFAULT NULL COMMENT '1审核成功 0代码提交失败 2.审核中  3.审核失败' AFTER `uploadcode`;
-
-SET FOREIGN_KEY_CHECKS=1;
-
--- ----------------------------
--- Records of fixture_data_vipmechanism
--- ----------------------------
+ALTER TABLE `fixture_conf_vipfunctionpoint` ADD COLUMN `type`  varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '类型 max has' AFTER `content`;
+ALTER TABLE `fixture_conf_vipfunctionpoint` ADD COLUMN `controller`  varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '控制器' AFTER `type`;
+ALTER TABLE `fixture_conf_vipfunctionpoint` ADD COLUMN `mehod`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '方法' AFTER `controller`;
+#插入会员机制数据
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('1', 'vip_max_site', '2', '2个', '0', '不限', '创建工地个数', 'max', null, null, '1', '2018-06-29 15:52:15');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('2', 'vip_dynamic', '2', '图文+VR', '2', '图文+VR+小视频', '发布工地动态', 'max', null, null, '1', '2018-06-29 15:52:18');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('3', 'vip_activity', '2', '每月2条', '0', '不限', '发布促销活动', 'max', null, null, '1', '2018-06-29 15:52:23');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('4', 'vip_has_participatory', '0', '无', '1', '有', '活动参与方式', 'has', 'ActivityController', null, '1', '2018-06-29 15:52:25');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('5', 'vip_appointment', '1', '每月一条', '1', '有', '客户预约', 'max', null, null, '1', '2018-06-29 15:52:27');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('6', 'vip_has_luck', '1', '有', '1', '有', '抽奖活动', 'has', 'ActivityLuckyController', null, '1', '2018-06-29 15:52:30');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('7', 'vip_has_freeoffer', '1', '有', '1', '有', '免费报价', 'allow', 'ClientAppointmentController', 'Appointment', '1', '2018-06-29 15:52:32');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('8', 'vip_has_message', '1', '有', '1', '有', '在线咨询', 'allow', null, null, '1', '2018-06-29 15:52:35');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('9', 'vip_has_consultation', '1', '有', '1', '有', '消息提醒', 'allow', null, null, '1', '2018-06-29 15:52:37');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('10', 'vip_has_visit_site', '1', '有', '1', '有', '预约参观', 'allow', 'ClientAppointmentController', 'Appointment', '1', '2018-06-29 15:52:39');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('11', 'vip_has_inviting_team_members', '1', '有', '1', '有', '邀请团队成员', 'allow', null, null, '1', '2018-06-29 15:52:41');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('12', 'vip_has_bussiness_comment', '1', '有', '1', '有', '业务评价', 'allow', null, null, '1', '2018-06-29 15:52:43');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('13', 'vip_has_share_more', '1', '有', '1', '有', '多渠道分享', 'allow', null, null, '1', '2018-06-29 15:52:46');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('14', 'vip_has_add_store', '1', '有', '1', '有', '新增门店', 'has', 'StoreController', null, '1', '2018-06-29 15:52:48');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('15', 'vip_has_add_admin', '0', '无', '1', '有', '新增用户', 'has', 'AdminController', null, '1', '2018-06-29 15:52:54');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('16', 'vip_has_role', '0', '无', '1', '有', '用户角色', 'has', 'RolesController', null, '1', '2018-06-29 15:52:58');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('17', 'vip_has_auth', '0', '无', '1', '有', '角色操作权限', 'has', 'RolesController', null, '1', '2018-06-29 15:53:00');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('18', 'vip_has_look', '0', '无', '1', '有', '角色视野权限', 'has', 'RolesController', null, '1', '2018-06-29 15:53:02');
+INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('19', 'vip_has_charts', '1', '简单数据分析', '2', '活动效果分析、客户转化率分析、渠道分析等更详细的数据分析、主流业务增长', '数据分析', 'max', null, null, '1', '2018-06-29 15:53:04');
+#添加会员版本数据源数据
 INSERT INTO `fixture_data_vipmechanism` VALUES ('1', '标准版', '1', '2018-06-25 15:36:42');
 INSERT INTO `fixture_data_vipmechanism` VALUES ('2', '专业版', '1', '2018-06-25 15:36:44');
--- ----------------------------
--- Records of fixture_conf_vipfunctionpoint
--- ----------------------------
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('1', 'vip_max_site', '2', '2个', '0', '不限', '创建工地个数', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('2', 'vip_dynamic', '2', '图文+VR', '2', '图文+VR+小视频', '发布工地动态', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('3', 'vip_activity', '2', '每月2条', '0', '不限', '发布促销活动', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('4', 'vip_participatory', '0', '无', '1', '有', '活动参与方式', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('5', 'vip_appointment', '1', '每月一条', '1', '有', '客户预约', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('6', 'vip_has_luck', '1', '有', '1', '有', '抽奖活动', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('7', 'vip_has_freeoffer', '1', '有', '1', '有', '免费报价', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('8', 'vip_has_message', '1', '有', '1', '有', '在线咨询', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('9', 'vip_has_consultation', '1', '有', '1', '有', '消息提醒', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('10', 'vip_has_visit_site', '1', '有', '1', '有', '预约参观', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('11', 'vip_has_inviting_team_members', '1', '有', '1', '有', '邀请团队成员', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('12', 'vip_has_bussiness_comment', '1', '有', '1', '有', '业务评价', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('13', 'vip_has_share_more', '1', '有', '1', '有', '多渠道分享', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('14', 'vip_has_add_store', '1', '有', '1', '有', '新增门店', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('15', 'vip_has_add_admin', '0', '无', '1', '有', '新增用户', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('16', 'vip_has_role', '0', '有', '1', '有', '用户角色', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('17', 'vip_has_auth', '0', '有', '1', '有', '角色操作权限', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('18', 'vip_has_look', '0', '有', '1', '有', '角色视野权限', '1', null);
-INSERT INTO `fixture_conf_vipfunctionpoint` VALUES ('19', 'vip_has_charts', '1', '简单数据分析', '1', '活动效果分析、客户转化率分析、渠道分析等更详细的数据分析、主流业务增长', '数据分析', '1', null);
 
 #删除工地成员
 DROP TABLE `fixture_site_participant`;
