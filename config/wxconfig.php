1d49e46988b1c8ceecf3fa9ab4de5a8a4933292e
舒全刚 <645064175@qq.com>
2018-07-06 18:48:13 +0800

    极光配置

--- config/wxconfig.php
+++ config/wxconfig.php
@@ -18,9 +18,9 @@ return [
     'secret'=>'e734776fd0c05c77daaab587bd0b0268',
     'url'=>'https://fixture.yygsoft.com/wx/authorize/back',//授权回调地址
     'requestdomain'=>['https://fixture.yygsoft.com'], //request合法域名
-    'wsrequestdomain'=>['wss://fixture.yygsoft.com'], //socket合法域名
-    'uploaddomain'=>['https://fixture.yygsoft.com'], //uploadFile合法域名
-    'downloaddomain'=>['https://fixture.yygsoft.com'],//downloadFile合法域名
+    'wsrequestdomain'=>['wss://fixture.yygsoft.com','wss://ws.im.jiguang.cn'], //socket合法域名
+    'uploaddomain'=>['https://fixture.yygsoft.com','https://sdk.im.jiguang.cn'], //uploadFile合法域名
+    'downloaddomain'=>['https://fixture.yygsoft.com','https://dl.im.jiguang.cn'],//downloadFile合法域名
     'address'=>'pages/index/index',//审核代码地址
     'tag'=>'装修',//审核代码标签
     'title'=>'装修',//小程序页面的标题
